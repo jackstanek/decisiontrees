cac1aec3520555bbe6f7287a9da5e2619f8d66fa
Jack Stanek <jrstanek@wisc.edu>
2025-02-08 16:41:40 -0600

    Progress on splitting, add graphviz dep

--- src/decisiontree.rs
+++ src/decisiontree.rs
@@ -1,47 +1,83 @@
 use std::collections::HashMap;
+use std::hash::Hash;
 
 use crate::feature::*;
 
 /// Binary decision tree
-pub struct DTree<T: Ord> {
-    root: Box<DNode<T>>,
+pub struct DTree<T: FeatureVector, L: Label> {
+    root: Box<DNode<T, L>>,
 }
 
 /// Interior node in a binary decision tree
-struct DNode<T: Ord> {
-    thresh: T,
-    lchild: Box<DNode<T>>,
-    rchild: Box<DNode<T>>,
+enum DNode<T: FeatureVector, L: Label> {
+    Internal {
+        ftr_idx: T::FeatureIdx,
+        thresh: T::Feature,
+        lchild: Box<DNode<T, L>>,
+        rchild: Box<DNode<T, L>>,
+    },
+    Leaf {
+        label: L,
+    },
 }
 
-impl<T: Ord> DTree<T> {}
+impl<T: FeatureVector, L: Label> DTree<T, L> {
+    pub fn build(features: &[impl FeatureVector]) -> Self {
+        todo!()
+    }
 
-/// Compute entropy of a
-fn entropy_binary<F: FeatureVector, L: Label>(
-    labeled_features: &[(F, L)],
-    idx: F::FeatureIdx,
-    thresh: F::Feature,
-) -> f64 {
-    let n_below = labeled_features
-        .iter()
-        .filter(|x| x.0.get_feature(idx) < thresh)
-        .count() as f64;
-    let n_above = labeled_features.len() as f64;
-    -(n_below * n_below.log2() + n_above * n_above.log2())
+    fn determine_candidate_split(&self) {
+
+    }
+
+    pub fn decide(&self, test_inst: &T) -> L {
+        let mut curr = &self.root;
+        loop {
+            match curr.as_ref() {
+                DNode::Internal {
+                    ftr_idx,
+                    thresh,
+                    lchild,
+                    rchild,
+                } => {
+                    curr = if test_inst.get_feature(ftr_idx) < *thresh {
+                        lchild
+                    } else {
+                        rchild
+                    }
+                }
+                DNode::Leaf { label } => return *label,
+            }
+        }
+    }
+}
+
+/// Compute entropy of some stream
+fn entropy_binary<C: Copy + Eq + Hash>(items: impl Iterator<Item = C>) -> f64 {
+    let item_counts: HashMap<C, usize> = items.fold(HashMap::new(), |mut m, i| {
+        *m.entry(i).or_insert(0) += 1_usize;
+        m
+    });
+    let total = item_counts.values().sum::<usize>() as f64;
+    -item_counts.values().fold(0.0, |ent, count| {
+        let p = *count as f64 / total;
+        ent + p * p.log2()
+    })
 }
 
 /// Calculate information entropy of a collection of feature vectors given a binary threshold
 fn info_gain_ratio_binary<F: FeatureVector, L: Label>(
-    features: &[(F, L)],
+    features: &[F],
+    labels: &[L],
     idx: F::FeatureIdx,
     thresh: F::Feature,
 ) -> f64 {
     let by_label: HashMap<L, Vec<F>> =
         features
             .iter()
-            .map(|x| (x.1, x.0.clone()))
+            .zip(labels)
             .fold(HashMap::new(), |mut m, x| {
-                m.entry(x.0).or_default().push(x.1);
+                m.entry(*x.1).or_default().push(x.0.clone());
                 m
             });
 
@@ -50,30 +86,43 @@ fn info_gain_ratio_binary<F: FeatureVector, L: Label>(
         let mut n_below = 0;
         let mut n_tot = 0;
         for fvec in fvecs.iter() {
-            if fvec.get_feature(idx) < thresh {
+            if fvec.get_feature(&idx) < thresh {
                 n_below += 1;
             }
             n_tot += 1;
         }
 
+        if n_tot == 0 {
+            continue;
+        }
+
         let p_below = n_below as f64 / n_tot as f64;
         cond_ent -= p_below * p_below.log2();
     }
-    cond_ent / entropy_binary(features, idx, thresh)
+    cond_ent / entropy_binary(labels.iter())
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::feature::{FeatureVector, Label};
+    use crate::{
+        decisiontree::info_gain_ratio_binary,
+        feature::{Feature, FeatureIdx, FeatureVector, Label},
+    };
 
     use super::entropy_binary;
 
+    impl Feature for f64 {}
+    impl FeatureIdx for bool {
+        fn enumerate() -> impl Iterator<Item = Self> {
+            [false, true].into_iter()
+        }
+    }
+
     impl FeatureVector for f64 {
         type Feature = f64;
+        type FeatureIdx = bool;
 
-        type FeatureIdx = usize;
-
-        fn get_feature(&self, _: Self::FeatureIdx) -> Self::Feature {
+        fn get_feature(&self, _: &Self::FeatureIdx) -> Self::Feature {
             *self
         }
     }
@@ -82,7 +131,8 @@ mod tests {
 
     #[test]
     fn test_bernoulli_entropy() {
-        let test: Vec<(f64, i64)> = (1..=10).map(|x| (x as f64 / 10.0, 1)).collect();
-        assert_eq!(entropy_binary(&test, 0, 0.5), 0.5);
+        let test: Vec<f64> = (1..=10).map(|x| (x as f64 / 10.0)).collect();
+        assert_eq!(entropy_binary(test.iter().map(|x| *x < 0.55)), 1.0);
+        assert_eq!(entropy_binary(test.iter().map(|_| true)), 0.0);
     }
 }
--- src/feature.rs
+++ src/feature.rs
@@ -1,23 +1,29 @@
 use std::hash::Hash;
 
-pub trait FeatureVector: Clone {
-    type Feature: Copy + PartialOrd;
-    type FeatureIdx: Copy + Eq + PartialEq + Hash;
+pub trait Feature: Copy + PartialOrd {}
 
-    fn get_feature(&self, idx: Self::FeatureIdx) -> Self::Feature;
+pub trait FeatureVector: Clone {
+    type Feature: Feature;
+    type FeatureIdx: FeatureIdx;
+
+    fn get_feature(&self, idx: &Self::FeatureIdx) -> Self::Feature;
+}
+
+pub trait FeatureIdx: Copy + Eq + PartialEq + Hash {
+    fn enumerate() -> impl Iterator<Item = Self>;
 }
 
 pub trait Label: Copy + Eq + Hash {}
 impl Label for u64 {}
 
-impl<T: Copy + PartialOrd, U: Copy> FeatureVector for (T, T, U) {
+impl<T: Feature, U: Copy> FeatureVector for (T, T, U) {
     type Feature = T;
     type FeatureIdx = TwoTupleIdx;
 
-    fn get_feature(&self, idx: Self::FeatureIdx) -> Self::Feature {
+    fn get_feature(&self, idx: &Self::FeatureIdx) -> Self::Feature {
         match idx {
             TwoTupleIdx::X1 => self.0,
-            TwoTupleIdx::X2 => self.1
+            TwoTupleIdx::X2 => self.1,
         }
     }
 }
@@ -26,4 +32,10 @@ impl<T: Copy + PartialOrd, U: Copy> FeatureVector for (T, T, U) {
 pub enum TwoTupleIdx {
     X1,
     X2,
+}
+
+impl FeatureIdx for TwoTupleIdx {
+    fn enumerate() -> impl Iterator<Item = Self> {
+        [Self::X1, Self::X2].into_iter()
+    }
 }
